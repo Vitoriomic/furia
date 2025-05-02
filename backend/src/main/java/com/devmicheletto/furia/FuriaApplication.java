70688a48748019fb30162575823a67eea69adc32
Vitorio Micheletto <vitoriomicheletto@gmail.com>
2025-05-02 20:44:02 -0300

    Fixing bugs with table creation in H2

--- backend/src/main/java/com/devmicheletto/furia/FuriaApplication.java
+++ backend/src/main/java/com/devmicheletto/furia/FuriaApplication.java
@@ -9,5 +9,4 @@ public class FuriaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FuriaApplication.class, args);
 	}
-
 }
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO time_furia (id, nome) VALUES (1, 'Counter-Strike 2');
+INSERT INTO time_furia (id, nome) VALUES (2, 'Valorant');
+INSERT INTO time_furia (id, nome) VALUES (3, 'Furia FC (Fut7)');
+INSERT INTO time_furia (id, nome) VALUES (4, 'League of Legends');
+
+INSERT INTO usuario (id, nome, data_nascimento, regiao, rede_social) VALUES (1, 'Vitório', '2003-02-12', 'Brasília', '@vitorio.mic');
+INSERT INTO usuario (id, nome, data_nascimento, regiao, rede_social) VALUES (2, 'Ana', '2002-03-15', 'Minas Gerais', '@aninha076');
+
+INSERT INTO usuario_times_favoritos (usuario_id, times_furia_id) VALUES (1, 1);
+INSERT INTO usuario_times_favoritos (usuario_id, times_furia_id) VALUES (1, 2);
+INSERT INTO usuario_times_favoritos (usuario_id, times_furia_id) VALUES (2, 3);
