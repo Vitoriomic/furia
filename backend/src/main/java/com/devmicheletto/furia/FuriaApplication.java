b15b81e71a394c5acbd5c4853b124e357f04ea53
Vitorio Micheletto <vitoriomicheletto@gmail.com>
2025-05-02 15:25:03 -0300

    Project created

---
+++ backend/src/main/java/com/devmicheletto/furia/FuriaApplication.java
@@ -0,0 +1,13 @@
+package com.devmicheletto.furia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuriaApplication.class, args);
+	}
+
+}
