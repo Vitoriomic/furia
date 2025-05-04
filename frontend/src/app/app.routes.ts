8b8e5559eaad3015b6adc6f0ed8918726dba8e1d
Vitorio Micheletto <vitoriomicheletto@gmail.com>
2025-05-04 01:25:53 -0300

    creating frontend structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GamesComponent } from './games/games.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { authGuard } from './auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'auth', component: AuthComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
+  { path: 'games', component: GamesComponent, canActivate: [authGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  { path: '**', redirectTo: 'auth' }
+];
